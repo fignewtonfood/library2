f022c79355c927d4dffd07f1c4a6bd2f0e46253a
Ike Mattice & Jose Torres <student@epicodus.com>
2015-08-26 15:14:46 -0700

    adds routes and html for librarian.html.twig and index.html.twig

---
+++ app/app.php
@@ -0,0 +1,51 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Book.php";
+    require_once __DIR__."/../src/Author.php";
+
+    $app = new Silex\Application();
+
+    $app['debug']=true;
+    $server = 'mysql:host=localhost;dbname=library_catalog';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+//Landing Page
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+//Librarian Home
+    //Loads basic page
+    $app->get("/librarian_home", function() use ($app) {
+        return $app['twig']->render('librarian.html.twig', array('books' => Book::getAll()));
+    });
+
+    //allows user to post new books and view them on the same page
+    $app->post("/librarian_home", function() use ($app){
+        $title = $_POST['title'];
+        $book = new Book($title);
+        $book -> save();
+        //this is where author link should go...?
+        return $app['twig']->render('librarian.html.twig', array('books' => Book::getAll()));
+    });
+
+    //allows user to delete all books
+    $app->post("/clear_library", function() use ($app) {
+        Book::deleteAll();
+        return $app['twig']->render('librarian.html.twig', array('books' => Book::getAll()));
+    });
+
+
+
+
+    return $app;
+ ?>
