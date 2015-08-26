cb5ef5b972e62aba1db6d02e9013a5bdb1ef9987
Ike Mattice & Jose Torres <student@epicodus.com>
2015-08-26 11:29:25 -0700

    add get and set functions as well as save(), getAll(), deleteAll(), and update() to Book.php.

---
+++ src/Book.php
@@ -0,0 +1,60 @@
+<?php
+class Book
+{
+    private $title;
+    private $id;
+
+    function __construct($title, $id=null)
+    {
+        $this->title = $title;
+        $this->id = $id;
+    }
+
+    function setTitle($new_title)
+    {
+        $this->title = (string) $new_title;
+    }
+
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO t_books (title) VALUES ('{$this->getTitle()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    function update($new_title)
+    {
+        $GLOBALS['DB']->exec("UPDATE t_books SET title = '{$new_title}' WHERE id = {$this->getId()};");
+        $this->setTitle($new_title);
+    }
+
+    static function getAll()
+    {
+        $returned_books = $GLOBALS['DB']->query("SELECT * FROM t_books;");
+        $books = array();
+        foreach($returned_books as $book) {
+            $title = $book['title'];
+            $id = $book['id'];
+            $new_book = new Book($title, $id);
+            array_push($books, $new_book);
+        }
+        return $books;
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM t_books;");
+    }
+
+
+}
+?>
--- tests/BookTest.php
+++ tests/BookTest.php
@@ -5,17 +5,154 @@
     */
 
     require_once 'src/Book.php';
-    require_once 'src/Author.php';
+    //require_once 'src/Author.php';
+    //require_once 'src/Patron.php';
 
     $server = 'mysql:host=localhost;dbname=library_catalog_test';
-$username = 'root';
-$password = 'root';
-$DB = new PDO($server, $username, $password);
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BookTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Book::deleteAll();
+            // Author::deleteAll();
+            // Patron::deleteAll();
+        }
+
+        function testGetTitle()
+        {
+            //Arrange
+            $title = "Grapes of Wrath";
+            $test_book = new Book($title);
+
+            //Act
+            $result = $test_book->getTitle();
+
+            //Assert
+            $this->assertEquals($title, $result);
+
+        }
+
+        function testSetTitle()
+        {
+            //Arrange
+            $title = "Grapes of Wrath";
+            $test_book = new Book($title);
+
+            //Act
+            $test_book->setTitle("Cannary Row");
+            $result = $test_book->getTitle();
+
+            //Assert
+            $this->assertEquals("Cannary Row", $result);
+
+        }
+
+        function testGetId()
+        {
+            //Arrange
+            $title = "Grapes of Wrath";
+            $id = 1;
+            $test_book = new Book($title, $id);
+
+            //Act
+            $result = $test_book->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
+
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $title = "Grapes of Wrath";
+            $id = 1;
+            $test_book = new Book($title, $id);
+
+            //Act
+            $test_book->save();
+
+            //Assert
+            $result = Book::getAll();
+            $this->assertEquals($test_book, $result[0]);
+
+        }
+
+        function testGetAll()
+        {
+            //Arrange
+            $title = "Grapes of Wrath";
+            $id = 1;
+            $test_book = new Book($title, $id);
+            $test_book->save();
+
+            $title2 = "Cannery Row";
+            $id2 = 2;
+            $test_book2 = new Book($title2, $id2);
+            $test_book2->save();
+
+            //Act
+            $result = Book::getAll();
+
+            //Assert
+            $this->assertEquals([$test_book, $test_book2], $result);
+        }
+
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $title = "Grapes of Wrath";
+            $id = 1;
+            $test_book = new Book($title, $id);
+            $test_book->save();
+
+            $title2 = "Cannery Row";
+            $id2 = 2;
+            $test_book2 = new Book($title2, $id2);
+            $test_book2->save();
+
+            //Act
+            Book::deleteAll();
+
+            //Assert
+            $result = Book::getAll();
+            $this->assertEquals([], $result);
+
+
+        }
+
+        function testUpdate()
+        {
+            //Arrange
+            $title = "Grapes of Wrath";
+            $test_book = new Book($title);
+            $test_book->save();
+
+            $new_title = "Harry Potter";
+
+            //Act
+            $test_book->update($new_title);
+
+            //Assert
+            $this->assertEquals("Harry Potter", $test_book->getTitle());
+        }
+
+        // function testGetAuthors()
+        // {
+        //
+        // }
+        //
+        // function testDelete()
+        // {
+        //
+        // }
 
 
 
-
-
-
-
+    }
  ?>
